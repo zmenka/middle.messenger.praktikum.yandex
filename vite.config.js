071a7c08ce6d0b1c7e8e854905fc4d648e6dde77
Evgeniia Briazgina <zmenka345@gmail.com>
2023-11-14 06:50:34 +0100

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import url from 'url';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    root: resolve(__dirname, 'client'),
-    publicDir: resolve(__dirname, 'static'),
-    build: {
-        outDir: resolve(__dirname, 'dist'),
-        emptyOutDir: true
-    },
-    css: {
-        postcss: {
-          plugins: [
-            autoprefixer({})
-          ],
-        }
-      }
-})
+  root: resolve(__dirname, 'client'),
+  publicDir: resolve(__dirname, 'static'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
+  }
+});
